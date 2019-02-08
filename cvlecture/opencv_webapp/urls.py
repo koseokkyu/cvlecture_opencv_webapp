8d7368bd56c5f6675f0d9aa67fb477a9c0a8cb7e
cvlecture <dighrh@naver.com>
2019-02-08 19:35:43 +0900

    first commit

---
+++ cvlecture/opencv_webapp/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+from django.conf import settings # add
+from django.conf.urls.static import static # add
+
+urlpatterns = [
+    url(r'^$', views.first_view, name='first_view'),
+    url(r'^uimage/$', views.uimage, name='uimage'),  # add
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # add
